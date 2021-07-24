a2f40d31d653c10e1ded52dd33bd3b75575c0251
sewoongkim <sewkim00@daum.net>
2021-07-24 12:52:37 +0900

    commit 3

--- unit4-3/3-5-01.py
+++ unit4-3/3-5-01.py
@@ -23,3 +23,50 @@ y_test = data_test[:, 0]
 # print(df_test)
 # print(data_test)
 
+import matplotlib.pyplot as plt
+label_dictionary =  {0:'T-shirt/top',1:'Trouser', 2:'Pullover',3: 'Dress', \
+    4:'Cost',5:'Sandal', 6:'Shirt', \
+    7:'Sneaker', 8:'Bag', 9:'Ankle boot'}
+
+i = 109
+plt.imshow(x_train[i].reshape(28,28), cmap='gray')
+plt.colorbar()
+plt.title('Label {} {}'.format(y_train[i], label_dictionary[y_train[i]]))
+plt.show()
+
+for i in range(9):
+    plt.subplot(3,3, i+1)
+    plt.imshow(x_train[i].reshape(28,28))
+    plt.colorbar()
+    plt.title('Label {}, {}'.format(y_train[i], label_dictionary[y_train[i]]))
+
+plt.tight_layout()
+plt.show()
+
+def AND(x1, x2):
+    w1,w2,threshold = 0.2, 0.2, 0.3
+    temp = w1 * x1 + w2 * x2
+    if temp <= threshold:
+        return 0
+    elif temp > threshold:
+        return 1
+
+def OR(x1, x2):
+    w1,w2,threshold = 0.3, 0.3, 0.2
+    temp = w1 * x1 + w2 * x2
+    if temp <= threshold:
+        return 0
+    elif temp > threshold:
+        return 1
+
+print(AND(0,0))
+print(AND(0,1))
+print(AND(1,0))
+print(AND(1,1))
+
+print(OR(0,0))
+print(OR(0,1))
+print(OR(1,0))
+print(OR(1,1))
+
+
